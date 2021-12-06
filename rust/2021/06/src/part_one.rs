abcf8d7e9fcc7ec7be3f10c88748ddaab3babe52
Hydrazer <explodingaerosal@gmail.com>
2021-12-06 16:40:57 -0700

    rust/2021/06

--- rust/2021/06/src/part_one.rs
+++ rust/2021/06/src/part_one.rs
@@ -1,3 +1,45 @@
-pub fn main(input: &String) {
+use std::collections::HashMap;
 
+pub fn main(input: &String) {
+  let mut fish_hash: HashMap<isize, isize> = HashMap::new();
+  let mut baby_hash: HashMap<isize, isize> = HashMap::new();
+
+  for num in input.split(",") {
+    *fish_hash.entry(num.parse::<isize>().unwrap()).or_insert(0) += 1
+  }
+
+  for _ in 0..80 {
+    fish_hash = fish_hash
+      .into_iter()
+      .filter(|(k, _)| k > &-1)
+      .map(|(k, v)| (k - 1, v))
+      .collect::<HashMap<_, _>>();
+
+    for (k, v) in &fish_hash.clone() {
+      if k == &-1_isize {
+        *fish_hash.entry(6).or_insert(0) += v;
+        fish_hash.insert(-1, 0);
+        *baby_hash.entry(9).or_insert(0) += v;
+      }
+    }
+
+    baby_hash = baby_hash
+      .into_iter()
+      .filter(|(k, _)| k > &-1)
+      .map(|(k, v)| (k - 1, v))
+      .collect::<HashMap<_, _>>();
+
+    for (k, v) in &baby_hash.clone() {
+      if k == &-1_isize {
+        *fish_hash.entry(6).or_insert(0) += v;
+        baby_hash.insert(-1, 0);
+        *baby_hash.entry(8).or_insert(0) += v;
+      }
+    }
+  }
+
+  println!(
+    "{}",
+    baby_hash.values().sum::<usize>() + fish_hash.values().sum::<usize>()
+  );
 }
