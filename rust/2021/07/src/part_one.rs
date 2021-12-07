a6d96953f5ad450ec0ae37fdfb4818c04916299c
Hydrazer <explodingaerosal@gmail.com>
2021-12-07 07:08:57 -0700

    rust/2021/07

--- rust/2021/07/src/part_one.rs
+++ rust/2021/07/src/part_one.rs
@@ -1,3 +1,22 @@
 pub fn main(input: &String) {
+  let pos_vec: Vec<usize> = input
+    .split(",")
+    .map(|s| s.parse::<usize>().unwrap())
+    .collect();
 
+  let check_pos_vec: Vec<usize> = (pos_vec.clone().into_iter().min().unwrap()..=
+    pos_vec.clone().into_iter().max().unwrap())
+    .map(|x| x)
+    .collect::<Vec<_>>();
+
+  let fuel_arr: Vec<usize> = check_pos_vec
+    .into_iter()
+    .map(|check_pos| {
+      pos_vec.clone().into_iter().map(|pos| {
+        (check_pos as isize - pos as isize).abs() as usize
+      }).sum::<usize>()
+    })
+    .collect::<Vec<_>>();
+
+  println!("{}", fuel_arr.iter().min().unwrap());
 }
--- rust/2021/07/src/part_two.rs
+++ rust/2021/07/src/part_two.rs
@@ -1,3 +1,28 @@
 pub fn main(input: &String) {
+  let pos_arr: Vec<usize> = input
+    .split(",")
+    .map(|s| s.parse::<usize>().unwrap())
+    .collect();
 
+  let check_pos_vec: Vec<usize> = (pos_arr.clone().into_iter().min().unwrap()..=
+    pos_arr.clone().into_iter().max().unwrap())
+    .map(|x| x)
+    .collect::<Vec<_>>();
+
+  let fuel_arr: Vec<usize> = check_pos_vec
+    .into_iter()
+    .map(|check_pos| {
+      pos_arr
+        .clone()
+        .into_iter()
+        .map(|pos| {
+          let diff = (check_pos as isize - pos as isize).abs();
+
+          (diff * (diff + 1) / 2) as usize
+        })
+        .sum::<usize>()
+    })
+    .collect::<Vec<_>>();
+
+  println!("{}", fuel_arr.iter().min().unwrap());
 }
